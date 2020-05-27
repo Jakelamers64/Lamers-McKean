150ac630c4e0f67bf52f7f8567f85047b2c80000
jake lamers <jakelamers931@gmail.com>
2020-05-27 15:55:24 -0500

    added folder for alpaca api practice stuff

---
+++ Alpaca/trader.py
@@ -0,0 +1,10 @@
+import config
+import alpaca_trade_api as tradeapi
+
+print("What is your KEY_ID")
+config.Key_ID = input()
+
+print("What is your SECRET_KEY")
+config.SECRET_KEY = input()
+
+print("Done")
