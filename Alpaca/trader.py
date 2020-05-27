b6ae2ad60824db5e2107489aa05de53bf199daad
jake lamers <jakelamers931@gmail.com>
2020-05-27 17:10:06 -0500

    created createOrder and getOrder functions

--- Alpaca/trader.py
+++ Alpaca/trader.py
@@ -1,8 +1,24 @@
-import config
-import requests
+import requests, config, json
 import alpaca_trade_api as tradeapi
 
-r = requests.get(config.ACCOUNT_URL,headers={'APCA-API-KEY-ID': config.KEY_ID,'APCA-API-SECRET-KEY': config.SECRET_KEY})
-print(r.content)
+def get_account():
+    r = requests.get(config.ACCOUNT_URL,headers=config.HEADERS)
+    return json.loads(r.content)
 
-print("Done")
+def create_order(symbol,qty,side,type,time_in_force):
+    data = {
+        "symbol": symbol,
+        "qty": qty,
+        "side": side,
+        "type": type,
+        "time_in_force": time_in_force
+    }
+
+    r = requests.post(config.ORDERS_URL,json=data,headers=config.HEADERS)
+
+    return json.loads(r.content)
+
+def get_orders():
+    r = requests.get(config.ORDERS_URL,headers=config.HEADERS)
+
+    return json.loads(r.content)
